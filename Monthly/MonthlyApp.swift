d20c972ca45816378e4a433e24b5e2e4feababf7
Jason Mitchell <devjmitchell@gmail.com>
2024-03-28 08:33:43 -0400

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Jason Mitchell on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
