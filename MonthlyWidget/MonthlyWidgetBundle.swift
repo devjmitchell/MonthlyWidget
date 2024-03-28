477e9fe3f4d616b9b5f068ad5e8fbda945b6d45e
Jason Mitchell <devjmitchell@gmail.com>
2024-03-28 08:38:35 -0400

    Create widget target

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Jason Mitchell on 3/28/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
